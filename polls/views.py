42becdcb00667e0f05407e01489a6af42a3e537f
Okan Koca <oknakoca@gmail.com>
2024-09-29 21:53:53 +0200

    started templating and rendering data

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Question, Choice
 
 
 def index(request):
-	return HttpResponse("hello world lol")
+	obj = Question.objects
+	return render(request, "polls/index.html", { "question": obj })
+
+def detail(request, question_id):
+	obj = get_object_or_404(Question, pk=question_id)
+	return render(request, "polls/detail.html", { "question": obj})
